09c74392ac3210a4973c995c440b609fde35c6de
Derek Johnston <Derek@dj-design.ca>
2020-03-21 14:36:23 -0400

    added form component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang='pug'>
+  .app
+    Form
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Form
   }
 }
 </script>
