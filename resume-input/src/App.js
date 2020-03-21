41d574f1a1ad30f73ed5e9fa1bad21e9d554e9dc
Kelly K H <kh.waterloo@gmail.com>
2020-03-21 13:07:10 -0400

    setting up the skeleton

---
+++ resume-input/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./app.scss";
+import Form from './Components/Form/Form';
+
+
+function App() {
+  return (
+    <section>
+      <Form></Form>
+    </section>
+  );
+}
+
+export default App;
