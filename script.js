1d5d51f6d9676d2d79e78d25b89c2e6da219a207
JWaltuch <jenna.waltuch@gmail.com>
2020-03-21 13:22:49 -0400

    wrote function that takes current inputs and loops through them, creating resume object

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-function createResume() {}
+// const fs = require('fs');
+
+function createResume() {
+  event.preventDefault();
+  let resume = {};
+  let inputs = [...document.getElementsByTagName('input')];
+  inputs.forEach(input => {
+    resume[input.name] = input.value;
+  });
+  // console.log(resume);
+  //window.location.href = "/success";
+}
