dbf443097d71b0ccb9b8dabed523dd113c78b2e7
JWaltuch <jenna.waltuch@gmail.com>
2020-03-21 13:31:17 -0400

    revised method for getting inputs, now by class

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 function createResume() {
   event.preventDefault();
   let resume = {};
-  let inputs = [...document.getElementsByTagName('input')];
+  let inputs = [...document.getElementsByClassName('basics')];
   inputs.forEach(input => {
     resume[input.name] = input.value;
   });
-  // console.log(resume);
+  console.log(resume);
   //window.location.href = "/success";
 }
