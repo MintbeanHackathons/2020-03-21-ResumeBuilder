52a11067f37ecc03e5dc18c560c2014fb5c67d83
aminamos <26092352+aminamos@users.noreply.github.com>
2020-03-21 09:56:48 -0700

    put JSON resume test data in TestComponent state

--- resume-builder/src/TestComponent.js
+++ resume-builder/src/TestComponent.js
@@ -2,8 +2,98 @@ import React, { Component } from 'react';
 
 class TestComponent extends Component {
   state = {
-    sayStuff: 'stuff'
-  };
+    "basics": {
+      "name": "John Doe",
+      "label": "Programmer",
+      "picture": "",
+      "email": "john@gmail.com",
+      "phone": "(912) 555-4321",
+      "website": "http://johndoe.com",
+      "summary": "A summary of John Doe...",
+      "location": {
+        "address": "2712 Broadway St",
+        "postalCode": "CA 94115",
+        "city": "San Francisco",
+        "countryCode": "US",
+        "region": "California"
+      },
+      "profiles": [{
+        "network": "Twitter",
+        "username": "john",
+        "url": "http://twitter.com/john"
+      }]
+    },
+    "work": [{
+      "company": "Company",
+      "position": "President",
+      "website": "http://company.com",
+      "startDate": "2013-01-01",
+      "endDate": "2014-01-01",
+      "summary": "Description...",
+      "highlights": [
+        "Started the company"
+      ]
+    }],
+    "volunteer": [{
+      "organization": "Organization",
+      "position": "Volunteer",
+      "website": "http://organization.com/",
+      "startDate": "2012-01-01",
+      "endDate": "2013-01-01",
+      "summary": "Description...",
+      "highlights": [
+        "Awarded 'Volunteer of the Month'"
+      ]
+    }],
+    "education": [{
+      "institution": "University",
+      "area": "Software Development",
+      "studyType": "Bachelor",
+      "startDate": "2011-01-01",
+      "endDate": "2013-01-01",
+      "gpa": "4.0",
+      "courses": [
+        "DB1101 - Basic SQL"
+      ]
+    }],
+    "awards": [{
+      "title": "Award",
+      "date": "2014-11-01",
+      "awarder": "Company",
+      "summary": "There is no spoon."
+    }],
+    "publications": [{
+      "name": "Publication",
+      "publisher": "Company",
+      "releaseDate": "2014-10-01",
+      "website": "http://publication.com",
+      "summary": "Description..."
+    }],
+    "skills": [{
+      "name": "Web Development",
+      "level": "Master",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "Javascript"
+      ]
+    }],
+    "languages": [{
+      "language": "English",
+      "fluency": "Native speaker"
+    }],
+    "interests": [{
+      "name": "Wildlife",
+      "keywords": [
+        "Ferrets",
+        "Unicorns"
+      ]
+    }],
+    "references": [{
+      "name": "Jane Doe",
+      "reference": "Reference..."
+    }]
+  }
 
   handleChange = (e) => {
     this.setState({
@@ -14,7 +104,7 @@ class TestComponent extends Component {
   render() {
     return (
       <div>
-        <p>{JSON.stringify(this.state.sayStuff)}</p>
+        <p>{JSON.stringify(this.state)}</p>
         <form>
           <input type="textarea" onChange={e => this.handleChange(e)}/>
           <button type="submit">Submit</button>
