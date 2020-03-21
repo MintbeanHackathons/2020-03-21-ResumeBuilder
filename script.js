02c0b6f6f33a48a5049addf50bf84d90ffbb80aa
JWaltuch <jenna.waltuch@gmail.com>
2020-03-21 14:54:54 -0400

    started to add function that would add more work when user clicks button, didn't finish

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ function createResume() {
   Object.keys(inputs).forEach(inputType => {
     //Loops over array of inputs for each type, and puts their
     //names and values in the resume object
-    //['company x',  ]
     let instance = {};
     inputs[inputType].forEach(input => {
       if (inputType === 'basics') {
@@ -56,3 +55,34 @@ function createResume() {
   console.log(resume);
   //window.location.href = "/success";
 }
+
+//NOT YET WORKING:
+//SHOULD ADD FORM FOR MORE WORK IF USER CLICKS BUTTON
+
+// function addMore() {
+//   let work = document.getElementById('work');
+//   //Create company div
+//   let company = document.createElement('div');
+//   company.innerText = 'Company: ';
+//   let cInput = document.createElement('input');
+//   cInput.class = 'work';
+//   cInput.name = 'company';
+//   cInput.type = 'text';
+//   company.appendChild(cInput);
+//   //Create position div
+//   let position = document.createElement('div');
+//   position.innerText = 'Position: ';
+//   let pInput = document.createElement('input');
+//   pInput.class = 'work';
+//   pInput.name = 'position';
+//   pInput.type = 'text';
+//   position.appendChild(pInput);
+//   //Create add button
+//   let button = document.createElement('button');
+//   button.innerText = 'Add Another';
+//   button.onclick = addMore;
+//   //Add all as children of work
+//   work.appendChild(company);
+//   work.appendChild(position);
+//   work.appendChild(button);
+// }
