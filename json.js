616a9d3c4171f8836c9e8c76bf9dcdde4e1ec6f7
Don Kim <dkim05@gmail.com>
2020-03-21 15:12:19 -0400

    Updated basics JSON fields

--- json.js
+++ json.js
@@ -1,14 +1,14 @@
 function submitform() {
-  var name = document.getElementById('jname').value;
-  var label = document.getElementById('jlabel').value;
   var j= {
     basics: {
-      name:this.document.getElementById('jname').value,
-      label:document.getElementById('jlabel').value
+      name:document.getElementById('jname').value,
+      label:document.getElementById('jlabel').value,
+      email:document.getElementById('jemail').value,
+      phone:document.getElementById('jphone').value,
+      website:document.getElementById('jwebsite').value,
+      summary:document.getElementById('jsummary').value
     }
   };
   var myJSON = JSON.stringify(j);
   document.write("<p>"+myJSON+"</p>");
-
-
 }
